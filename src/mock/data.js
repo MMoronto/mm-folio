af537e01395911f93f04bc70fc08d241c3dcbedb
MMORONTO <t2wycemo@gmail.com>
2021-09-14 12:30:24 -0400

    style modification

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,17 +2,17 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Mobola | Developer', // e.g: 'Name | Developer'
+  lang: 'en, fr', // e.g: en, es, fr, jp
+  description: 'Welcome', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'I am',
+  name: 'Mobola Moronto',
+  subtitle: 'Software Engineer',
+  cta: 'know more',
 };
 
 // ABOUT DATA
@@ -21,37 +21,37 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://docs.google.com/document/d/1U5V4hgE2VYTVNusCUeSbDYt1q88Jm5VggpOdISnx3zE/edit?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'robodex.jpg',
+    title: 'Robo-Dex',
+    info: 'Robo-Dex is a rolodex for organizing your robot contacts. A simple React app that implements basic sort and search functionality, pulls data from a simple backend API with responsive CSS styling.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://mmoronto.github.io/Robo-dex/',
+    repo: 'https://github.com/MMoronto/Robo-dex', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'covidDash.jpg',
+    title: 'Covid-19 Dashboard',
+    info: 'A dashboard for tracking global covid 19 cases. A simple React-Leaflet application.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://covid-19-dashboard-ce10d.web.app/',
+    repo: 'https://github.com/MMoronto/covid-19-dashboard', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'escapevelocity.jpg',
+    title: 'Escape Velocity',
+    info: 'Commercial Space carrier (capsule/launch vehicles) review application. Ruby on Rails and React.js.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://github.com/MMoronto/escape-velocity',
+    repo: 'https://github.com/MMoronto/escape-velocity', // if no repo, the button will not show up
   },
 ];
 
@@ -68,27 +68,27 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/MMoronto',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'hashnode',
+      url: 'https://hashnode.com/@Morakinyo',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/mobola-moronto-02548742/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/MMoronto?tab=overview&from=2021-07-01&to=2021-07-22',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
